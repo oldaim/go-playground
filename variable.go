cded72f9121f916e1766cd48177a95249b7b5bd8
donggyun.kim-at-353028931260 <donggyun.kim@eigene.ai>
2024-12-18 16:03:28 +0900

    변수 선언

---
+++ variable.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//변수 선언
+	var name string = "john"
+	var age int = 20
+
+	//변수 선언 (단축형)
+	name2 := "john"
+	age2 := 20
+
+	fmt.Println(name, age)
+	fmt.Println(name2, age2)
+}
